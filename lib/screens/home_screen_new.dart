59ab9f7d3db5d9196e999b8ef8469b737f42d050
yuubinnkyoku <yuubinnkyoku0730@gmail.com>
2025-06-15 11:51:59 +0900

    ｓ

--- lib/screens/home_screen_new.dart
+++ lib/screens/home_screen_new.dart
@@ -1,25 +1,20 @@
+
 import 'package:flutter/material.dart';
-import 'package:shojin_app/widgets/next_contest_card.dart'; // Add this line
 
 class NewHomeScreen extends StatelessWidget {
   const NewHomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( // Remove const
+    return const Scaffold(
       // appBar: AppBar(
       //   title: const Text('ホーム'),
       // ),
-      body: ListView( // Change to ListView to accommodate multiple widgets
-        children: const [
-          NextContestCard(), // Add this line
-          Center(
-            child: Text(
-              '新しいホーム画面',
-              style: TextStyle(fontSize: 24),
-            ),
-          ),
-        ],
+      body: Center(
+        child: Text(
+          '新しいホーム画面',
+          style: TextStyle(fontSize: 24),
+        ),
       ),
     );
   }
